1865413afbb05db5b8d3ccb1da658775bf26d80d
Eve Stockstill <evestockstillpdx@gmail.com>
2020-02-04 22:18:26 -0800

    made character hook

--- src/hooks/characters.js
+++ src/hooks/characters.js
@@ -1,2 +1,15 @@
 import { useState, useEffect } from 'react';
-import { getCharacters, getStatus } from '../services/rickMortyApi';
+import { getCharacters } from '../services/rickMortyApi';
+
+export const useCharacters = searchStatusTerm => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    if(!searchStatusTerm) return;
+
+    getCharacters(searchStatusTerm)
+      .then(setCharacters);
+  }, [searchStatusTerm]);
+
+  return characters;
+};
